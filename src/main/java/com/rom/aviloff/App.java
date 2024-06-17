c4b3ff7d7596119612569ba09c0780960f8dbea6
Авилов Роман Игоревич <aviloff.rom@yandex.ru>
2024-06-17 15:30:31 +0300

    initial commit

---
+++ src/main/java/com/rom/aviloff/App.java
@@ -0,0 +1,13 @@
+package com.rom.aviloff;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
